a279bc362f0b69e9fe055d143305dd0695799ad1
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2021-12-01 23:38:08 +0700

    Delete notification_route.js

--- notification_route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const notification = require('../handler/notification/newgame_notifcation');
-
-router.post('/push', notification.notifyNewGame);
-
-module.exports = router;
