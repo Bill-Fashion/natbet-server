062b3b55460762df37486be98a70ebbcf0a1d0c3
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2021-12-01 23:09:12 +0700

    Add files via upload

--- routes/result_route.js
+++ routes/result_route.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const result = require('../handler/result/firebase_read');
 
 router.post('/pay', result.firebaseResult);
+router.post('/refund', result.refundBet);
+router.post('/setTimeout', result.setTimeoutToClose);
+router.post('/setIntervalCoins', result.setIntervalCoins);
 
 module.exports = router;
