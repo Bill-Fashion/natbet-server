c32ac014435efa6f19faef8234562de4d6f34e2d
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2021-12-03 12:05:15 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const routes = require('./routes/index');
 global.admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
+const http = require("http");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -13,6 +14,11 @@ admin.initializeApp({
 app.use(cors());
 app.use(express.json());
 routes(app);
+
+//Ping heroku every 20p to keep server awake
+setInterval(function() {
+    http.get("https://natbet.herokuapp.com");
+}, 1200000); // every 20 minutes (1200000)
 
 app.listen(PORT, () => {
   console.log(`Server app listening at http://localhost:${PORT}`)
